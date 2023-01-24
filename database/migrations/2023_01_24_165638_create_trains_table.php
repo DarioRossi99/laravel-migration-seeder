504a4df6866b7c8c666da02d6674a3af70accaf3
DarioRossi99 <rssdra99@gmail.com>
2023-01-24 19:02:31 +0100

    fine

--- database/migrations/2023_01_24_165638_create_trains_table.php
+++ database/migrations/2023_01_24_165638_create_trains_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('company');
+            $table->string('departure_station');
+            $table->string('arrival_station');
+            $table->datetime('departure_time');
+            $table->datetime('arrival_time');
+            $table->string('train_code');
+            $table->string('number_of_carriages');
+            $table->boolean('on_time')->default(1);
+            $table->boolean('cancelled')->default(0);
             $table->timestamps();
         });
     }
@@ -26,6 +35,18 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('trains');
+        Schema::dropIfExists('trains', function (Blueprint $table) {
+            $table->id();
+            $table->dropColumn('company');
+            $table->dropColumn('departure_station');
+            $table->dropColumn('arrival_station');
+            $table->dropColumn('departure_time');
+            $table->dropColumn('arrival_time');
+            $table->dropColumn('train_code');
+            $table->dropColumn('number_of_carriages');
+            $table->dropColumn('on_time')->default(1);
+            $table->dropColumn('cancelled')->default(0);
+            $table->timestamps();
+        });
     }
 };
